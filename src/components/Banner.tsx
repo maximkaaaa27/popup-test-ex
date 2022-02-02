cf5514ec65bb5a1e2d696704468e90a9fa400e36
maksimka <max_005_27@mail.ru>
2022-02-03 00:14:57 +0300

    addBannerView

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-
+import qrCode from '../assets/qr-code.svg'
 
 export const Banner = () => (
   <div className='banner'>
-
-    <h2>ИСПОЛНИТЕ МЕЧТУ ВАШЕГО МАЛЫША!</h2>
-    <h2>ПОДАРИТЕ ЕМУ СОБАКУ!</h2>
-    <img src="..." alt="qr-code" />
-    
-    <p>Сканируйте QR-код или нажмите OK</p>
-    <button>OK</button>
-
+    <div className='contains'>
+      <div className='title'>
+        <div className='title_first'> ИСПОЛНИТЕ МЕЧТУ ВАШЕГО МАЛЫША!</div>
+        <div className='title_second'> ПОДАРИТЕ ЕМУ СОБАКУ!</div>
+      </div>
+      <img src={qrCode} alt="qr-code" />      
+      <div className='text_qr_code'>Сканируйте QR-код или нажмите OK</div>
+      <div className='contains_button' onClick={() => console.log('object')}>
+        <span>OK</span>
+      </div>
+    </div>
   </div>
 )
